a44e19b321cba21e81666a1a2be5b4b240d5b2bb
孙权 <sunquanlove1964@gmail.com>
2016-04-20 18:13:08 +0800

    no message

--- FlowLabel/app/src/main/java/com/example/sunquan/flowlabel/MainActivity.java
+++ FlowLabel/app/src/main/java/com/example/sunquan/flowlabel/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private FlowLabelLayout center_flowlayout;
     private FlowLabelLayout left_flowlayout;
 
-    private List<String> labelList = new ArrayList<String>();
+    private List<String> centerlabelList = new ArrayList<String>();
+
+    private List<String> leftlabelList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         initData();
         setViewData();
     }
-    
+
     private void initView() {
         setContentView(R.layout.activity_main);
         center_flowlayout = (FlowLabelLayout) this.findViewById(R.id.center_flowlayout);
@@ -35,24 +37,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        labelList.add("爱笑的她");
-        labelList.add("理想一个归期");
+        centerlabelList.add("爱笑的她");
 
-        labelList.add("茶底世界");
-        labelList.add("睡了吗");
-        labelList.add("如此美妙");
+        centerlabelList.add("如此美妙");
 
-        labelList.add("她的眼睛 闪亮如灯火");
-        labelList.add("女孩别走 你落下了我");
-        labelList.add("岁月 你别催");
-        labelList.add("该来的我不推");
+        centerlabelList.add("她的眼睛 闪亮如灯火");
+        centerlabelList.add("女孩别走 你落下了我");
+        centerlabelList.add("岁月 你别催");
+        centerlabelList.add("该来的我不推");
 
-        labelList.add("该还的我还");
-        labelList.add("不觉被迷惑");
+        centerlabelList.add("该还的我还");
+        centerlabelList.add("不觉被迷惑");
 
-        labelList.add("她看着我");
-        labelList.add("像在说什么");
-        labelList.add("难道她也注意到我");
+        centerlabelList.add("她看着我");
+        centerlabelList.add("像在说什么");
+        centerlabelList.add("难道她也注意到我");
+
+
+        leftlabelList.add("停了的钟");
+
+        leftlabelList.add("他不懂");
+
+        leftlabelList.add("裂缝中的阳光");
+        leftlabelList.add("心底的影子");
+        leftlabelList.add("愿你的故事细水长流");
+        leftlabelList.add("古风醉故人 故人非良人");
+
+        leftlabelList.add("放任潇洒");
+        leftlabelList.add("终成无畏");
+
+        leftlabelList.add("唱歌的人不许掉眼泪");
+        leftlabelList.add("在人间");
+        leftlabelList.add("一个人孩子的心愿");
+
     }
 
     private void setViewData() {
@@ -62,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
         lp.topMargin = 15;
         lp.bottomMargin = 15;
 
-        for (int i = 0; i < labelList.size(); i++) {
+        for (int i = 0; i < centerlabelList.size(); i++) {
             TextView view = new TextView(this);
-            String lable_str = labelList.get(i);
+            String lable_str = centerlabelList.get(i);
             view.setText(lable_str);
 
             view.setBackgroundDrawable(getResources().getDrawable(R.drawable.lable_textview_bg));
@@ -75,9 +92,9 @@ public class MainActivity extends AppCompatActivity {
             center_flowlayout.requestLayout();
         }
 
-        for (int i = 0; i < labelList.size(); i++) {
+        for (int i = 0; i < leftlabelList.size(); i++) {
             TextView view = new TextView(this);
-            String lable_str = labelList.get(i);
+            String lable_str = leftlabelList.get(i);
             view.setText(lable_str);
 
             view.setBackgroundDrawable(getResources().getDrawable(R.drawable.lable_textview_bg));
