12888e47e64a13c1c1c3183c1c293391545033cc
孙权 <sunquanlove1964@gmail.com>
2016-05-03 18:08:08 +0800

    no message

--- FlowLabel/app/src/main/java/com/example/sunquan/flowlabel/MainActivity.java
+++ FlowLabel/app/src/main/java/com/example/sunquan/flowlabel/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+
         initData();
         setViewData();
     }
